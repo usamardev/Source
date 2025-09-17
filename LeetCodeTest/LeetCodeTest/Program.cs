662608b21b58ab5ddf470d46be2a1d65834660bb
Samar <samar.ubaydullayev@gmail.com>
2025-09-17 17:53:44 +0500

    treqq

--- LeetCodeTest/LeetCodeTest/Program.cs
+++ LeetCodeTest/LeetCodeTest/Program.cs
@@ -13,17 +13,6 @@
             hashmap.ContainsNearbyDuplicate([1, 2, 3, 1, 2, 3],2);
             Console.WriteLine("Hello, World!");
 
-
-           
-
-            //string[] words = ["Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do"];
-            //int maxWidth = 20;
-
-            //test.FullJustify(words, maxWidth);  
-
-            //string postText = "ABCD";
-            //string result=test.Convert(postText, 2);
-            //Console.WriteLine(result);
         }
         
 

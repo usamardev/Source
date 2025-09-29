beb71a0e4b16df42d0979d957f3eb1eb90597c26
Samar <samar.ubaydullayev@gmail.com>
2025-09-29 14:33:35 +0500

    ChangeNameControllerNameAut

---
+++ src/WebApp/Auth/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

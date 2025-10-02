4fc2f5376a79c23f4b053c74630697dd17d8f7ae
Samar <samar.ubaydullayev@gmail.com>
2025-10-02 12:29:49 +0500

    AverageOfLevels

--- LeetCodeTest/LeetCodeTest/BinaryTreeBFS.cs
+++ LeetCodeTest/LeetCodeTest/BinaryTreeBFS.cs
@@ -31,11 +31,30 @@
             return result;
         }
 
-        public IList<int> RightSideView1(TreeNode root)
+        public IList<double> AverageOfLevels(TreeNode root)
         {
-            List<int> result = new List<int>();
+            List<double> result = new List<double>();
             if (root == null) return result;
 
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                int size = queue.Count;
+                double sum = 0;
+
+                for (int i = 0; i < size; i++)
+                {
+                    TreeNode node = queue.Dequeue();
+                    sum += node.val;
+
+                    if (node.left != null) queue.Enqueue(node.left);
+                    if (node.right != null) queue.Enqueue(node.right);
+                }
+
+                result.Add(sum / size);
+            }
 
             return result;
         }

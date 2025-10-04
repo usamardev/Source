e8fc744ae458949aacc8b28a8b21dd57fe1678e7
usamardev <samar.ubaydullayev@gmail.com>
2025-10-04 16:49:42 +0500

    SQRT

--- LeetCodeTest/LeetCodeTest/MathALG.cs
+++ LeetCodeTest/LeetCodeTest/MathALG.cs
@@ -41,6 +41,30 @@ namespace LeetCodeTest
             return count;
         }
 
-        
+        public int MySqrt(int x)
+        {
+            if (x < 2) return x;
+
+            int left = 1, right = x / 2, ans = 0;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                long sq = (long)mid * mid; // overflowdan saqlanish uchun long
+                if (sq == x) return mid;
+                if (sq < x)
+                {
+                    ans = mid;
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return ans;
+        }
     }
 }

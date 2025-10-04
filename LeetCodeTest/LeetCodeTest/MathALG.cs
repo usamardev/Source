3aad96e91376159d5849392f7f295a0c6fd22343
usamardev <samar.ubaydullayev@gmail.com>
2025-10-04 16:01:05 +0500

    PlusOne

--- LeetCodeTest/LeetCodeTest/MathALG.cs
+++ LeetCodeTest/LeetCodeTest/MathALG.cs
@@ -8,5 +8,21 @@ namespace LeetCodeTest
 {
     public class MathALG
     {
+        public int[] PlusOne(int[] digits)
+        {
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (digits[i] != 9)
+                {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+
+            int[] newNumber = new int[digits.Length + 1];
+            newNumber[0] = 1;
+            return newNumber;
+        }
     }
 }

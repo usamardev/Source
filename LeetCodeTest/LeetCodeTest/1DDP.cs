6ff31f34a6a85eee25de51dff77194aaa75b2edb
usamardev <samar.ubaydullayev@gmail.com>
2025-10-05 19:23:21 +0500

    1DDPAddClass

---
+++ LeetCodeTest/LeetCodeTest/1DDP.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeTest
+{
+    public class _1DDP
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n <= 0)
+                return 0;
+            if (n == 1)
+                return 1;
+
+            int fib1 = 1;
+            int fib2 = 2;
+
+            for (int i = 3; i <= n; i++)
+            {
+                int fib = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = fib;
+            }
+            return fib2;
+        }
+    }
+}

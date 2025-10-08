8968df01d923d6056a72d6cda650bb34d71f7e35
Samar <samar.ubaydullayev@gmail.com>
2025-10-08 11:09:36 +0500

    SearchMatrix74

--- LeetCodeTest/LeetCodeTest/BinarySearch.cs
+++ LeetCodeTest/LeetCodeTest/BinarySearch.cs
@@ -20,5 +20,20 @@
 
             return left;
         }
+
+        //74. Search a 2D Matrix
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            if (matrix.Length == 0 && matrix[0].Length==0) return false;
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if(matrix[i][j] == target) return true;
+                }
+            }
+            return false;
+        }
     }
 }

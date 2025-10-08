6ee0afdde9fa366d27cb79c8011718bb42db7a52
Samar <samar.ubaydullayev@gmail.com>
2025-10-08 11:43:21 +0500

    FindMinAnotherWay

--- LeetCodeTest/LeetCodeTest/BinarySearch.cs
+++ LeetCodeTest/LeetCodeTest/BinarySearch.cs
@@ -100,5 +100,25 @@
             }
             return min;
         }
+        public int FindMinAnotherWay(int[] nums)
+        {
+            int l = 0;
+            int r = nums.Length - 1;
+
+            while (l < r)
+            {
+                int m = l + (r - l) / 2;
+
+                if (nums[m] < nums[r])
+                {
+                    r = m;
+                }
+                else
+                {
+                    l = m + 1;
+                }
+            }
+            return nums[l];
+        }
     }
 }

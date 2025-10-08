15549208b0d2804c2ae28c77a740356141c42125
Samar <samar.ubaydullayev@gmail.com>
2025-10-08 12:35:42 +0500

    AddNewClassMultidimensionalDP

---
+++ LeetCodeTest/LeetCodeTest/MultidimensionalDP.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeTest
+{
+    public class MultidimensionalDP
+    {
+        public int MinimumTotal(IList<IList<int>> triangle)
+        {
+            int min = 0;
+
+            foreach (List<int> item in triangle)
+            {
+                min += item.Min();
+            }
+            return min;
+
+            //int[] lastItems = triangle[triangle.Count - 1].ToArray();
+            //for (int row = triangle.Count - 2; row >= 0; row--)
+            //{
+            //    for (int col = 0; col < triangle[row].Count; col++)
+            //    {
+            //        lastItems[col] = triangle[row][col] + Math.Min(lastItems[col], lastItems[col + 1]);
+            //    }
+            //}
+            //return lastItems[0];
+        }
+    }
+}

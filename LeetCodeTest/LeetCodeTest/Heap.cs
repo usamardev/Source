d0307ee58be8e10cdc22eb1c3fa887ad10d0e126
Samar <samar.ubaydullayev@gmail.com>
2025-10-13 12:44:59 +0500

    373. Find K Pairs with Smallest Sums

--- LeetCodeTest/LeetCodeTest/Heap.cs
+++ LeetCodeTest/LeetCodeTest/Heap.cs
@@ -16,5 +16,31 @@
 
             return pq.Peek(); // heap ichidagi eng kichik — k-chi eng katta
         }
+
+        public IList<IList<int>> KSmallestPairs(int[] nums1, int[] nums2, int k)
+        {
+            var result = new List<IList<int>>();
+            if (nums1.Length == 0 || nums2.Length == 0 || k == 0)
+                return result;
+            var pq = new PriorityQueue<(int i, int j), int>();
+
+            for (int i = 0; i < Math.Min(nums1.Length, k); i++)
+            {
+                pq.Enqueue((i, 0), nums1[i] + nums2[0]);
+            }
+
+            while (k-- > 0 && pq.Count > 0)
+            {
+                var (i, j) = pq.Dequeue();
+                result.Add(new List<int> { nums1[i], nums2[j] });
+
+                if (j + 1 < nums2.Length)
+                {
+                    pq.Enqueue((i, j + 1), nums1[i] + nums2[j + 1]);
+                }
+            }
+
+            return result;
+        }
     }
 }

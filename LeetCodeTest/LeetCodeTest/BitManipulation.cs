b0c2893e368b732e24b9ec12e02b54d48fa8bfd5
Samar <samar.ubaydullayev@gmail.com>
2025-10-14 11:42:42 +0500

    191. Number of 1 Bits

--- LeetCodeTest/LeetCodeTest/BitManipulation.cs
+++ LeetCodeTest/LeetCodeTest/BitManipulation.cs
@@ -25,6 +25,19 @@
 
             return result;
         }
+
+        public int HammingWeight(int n)
+        {
+            int count = 0;
+
+            for(int i = 0;i < 32;i++)
+            {
+                if ((n & 1) == 1)
+                    count++;
+                n>>= 1;
+            }
+            return count;
+        }
     }
 
 }

2a70df248e931e9102b707795baa7fadb6952cf5
Samar <samar.ubaydullayev@gmail.com>
2025-10-14 09:05:31 +0500

    BitReset

--- LeetCodeTest/LeetCodeTest/BitManipulation.cs
+++ LeetCodeTest/LeetCodeTest/BitManipulation.cs
@@ -2,6 +2,29 @@
 {
     public class BitManipulation
     {
+        public int ReverseBitsInt(int n)
+        {
+            long r = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                r = (r << 1) | (n & 1);
+                n >>= 1;
+            }
+            return (int)r; // agar siz natijani signed int koʻrmoqchi bo'lsangiz
+        }
+
+        public int ReverseBitsBetter(int n)
+        {
+            var result = 0;
+
+            for (int i = 0; i < 32; i++)
+            {
+                var bit = (n >> i) & 1;
+                result = result | (bit << 31 - i);
+            }
+
+            return result;
+        }
     }
 
 }

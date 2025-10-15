580a4777ad00d30244a8dc9ea196b4d6454cc325
Samar <samar.ubaydullayev@gmail.com>
2025-10-15 16:25:48 +0500

    AddnewClassKadanesAlgorithm

---
+++ LeetCodeTest/LeetCodeTest/KadanesAlgorithm.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCodeTest
+{
+    public class KadanesAlgorithm
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int currentSum = nums[0];
+            int maxSum = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                currentSum = Math.Max(nums[i], currentSum + nums[i]);
+                maxSum = Math.Max(maxSum, currentSum);
+            }
+
+            return maxSum;
+        }
+    }
+}

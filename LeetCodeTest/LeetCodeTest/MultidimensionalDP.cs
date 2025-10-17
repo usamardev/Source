dfeee3cbeff5dba06c54d28f5e21e64743ac3a59
Samar <samar.ubaydullayev@gmail.com>
2025-10-17 12:18:18 +0500

    221. Maximal Square

--- LeetCodeTest/LeetCodeTest/MultidimensionalDP.cs
+++ LeetCodeTest/LeetCodeTest/MultidimensionalDP.cs
@@ -227,5 +227,33 @@
             return profit;
         }
 
+        //221. Maximal Square
+        public int MaximalSquare(char[][] matrix)
+        {
+            if (matrix.Length == 0 || matrix[0].Length == 0)
+                return 0;
+
+            int m = matrix.Length, n = matrix[0].Length;
+            int[,] dp = new int[m + 1, n + 1];
+            int maxSide = 0;
+
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (matrix[i - 1][j - 1] == '1')
+                    {
+                        dp[i, j] = Math.Min(
+                            Math.Min(dp[i - 1, j], dp[i, j - 1]),
+                            dp[i - 1, j - 1]
+                        ) + 1;
+                        maxSide = Math.Max(maxSide, dp[i, j]);
+                    }
+                }
+            }
+
+            return maxSide * maxSide;
+        }
+
     }
 }
